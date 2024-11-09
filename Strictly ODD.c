a6defbdf951eeee9aa22532ac0229798d659bc35
thanujayarabolu <thanujayarabolu1504@gmail.com>
2024-11-09 15:01:07 +0530

    Uploading code: Strictly ODD.c

---
+++ Strictly ODD.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int A[n];
+    for(i=0 ; i<n ; i++){
+        scanf("%d",&A[i]);
+        if(i%2==0 && A[i]%2!=0){
+            c=1;
+        }
+    }if(c==1){
+        printf("False");
+    }else{
+        printf("True");
+    }
+}
