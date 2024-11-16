bc68c1be2901493c93e0aacfc9deb5d465deebc5
thanujayarabolu <thanujayarabolu1504@gmail.com>
2024-11-16 16:56:36 +0530

    Uploading code: N - S - C.c

---
+++ N - S - C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,i,c,d;
+    scanf("%d%d",&a,&b);
+    for(i=a+1 ; i<b ;i++){
+        c=i*i;
+        d=i*i*i;
+        printf("%d %d %d",i,c,d);
+        printf("\n");
+    }
+}
