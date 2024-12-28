bd49a10adec660a327e40f067bf1c60e7f7ee564
thanujayarabolu <thanujayarabolu1504@gmail.com>
2024-12-28 21:18:08 +0530

    Uploading code: Odd Sum.c

---
+++ Odd Sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int A[n];
+    for(i=0 ; i<n ; i++){
+        scanf("%d",&A[i]);
+    }
+    for(i=0 ; i<n ; i++){
+        if(A[i]%2==1){
+            sum+=A[i];
+        }
+    }
+    printf("%d",sum);
+}
